2f47fb0fc1c702edc159512f273ff8d1c2ebcf8e
Chi <yochi080684@gmail.com>
2022-08-16 00:47:33 +0800

    Update 2022-08-15T16:47:31.274Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,77 @@
 "use strict";
 
-var scrollContainer = document.querySelector(".item-tag ul");
-
-if (scrollContainer) {
-  scrollContainer.addEventListener("wheel", function (evt) {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-  });
-}
-
+// 手刻 item-tag
+// const scrollContainer = document.querySelector(".item-tag ul");
+// if (scrollContainer) {
+//     scrollContainer.addEventListener("wheel", (evt) => {
+//         evt.preventDefault();
+//         scrollContainer.scrollLeft += evt.deltaY;
+//     });
+// }
 document.querySelector('[data-bs-toggle="modal"]').onclick = function () {
   return document.querySelector('.navbar-collapse').classList.remove("show");
 };
+
+var programSwiper = new Swiper(".programSwiper", {
+  loop: true,
+  slidesPerView: "auto",
+  loopedSlides: 17,
+  centeredSlides: false,
+  grabCursor: true,
+  mousewheel: true,
+  breakpoints: {
+    320: {
+      spaceBetween: 8
+    },
+    768: {
+      spaceBetween: 16
+    }
+  }
+});
+var imageSwiper = new Swiper(".imageSwiper", {
+  cssMode: true,
+  slidesPerView: 'auto',
+  loop: true,
+  autoplay: true,
+  disableOnInteraction: false,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev"
+  },
+  pagination: {
+    el: ".feedback-controller",
+    bulletElement: 'a',
+    clickable: true
+  }
+});
+var studentSwiper = new Swiper(".studentSwiper", {
+  cssMode: true,
+  slidesPerView: 'auto',
+  loop: true,
+  autoplay: true,
+  disableOnInteraction: false,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev"
+  },
+  pagination: {
+    el: ".feedback-controller",
+    bulletElement: 'a',
+    clickable: true
+  }
+});
+var courseSwiper = new Swiper(".courseSwiper", {
+  slidesPerView: "auto",
+  centeredSlides: false,
+  grabCursor: true,
+  mousewheel: true,
+  breakpoints: {
+    320: {
+      spaceBetween: 8
+    },
+    768: {
+      spaceBetween: 16
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
