d2250b6f3ff1fd8c33384a83ac8a4bed6096bd13
Chi <yochi080684@gmail.com>
2022-08-15 17:55:23 +0800

    html css completed

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,10 @@
 const scrollContainer = document.querySelector(".item-tag ul");
 
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
+if(scrollContainer){
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+}
+
+document.querySelector('[data-bs-toggle="modal"]').onclick = () => document.querySelector('.navbar-collapse').classList.remove("show");
