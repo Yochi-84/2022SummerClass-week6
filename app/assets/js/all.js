da53593b6d9b14d3611ec895e9f2bbcf2e8e37d8
Chi <yochi080684@gmail.com>
2022-08-17 13:50:18 +0800

    js update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -72,6 +72,15 @@ let courseSwiper = new Swiper(".courseSwiper", {
   }
 });
 
+if (document.querySelector('#tag')) {
+  Array.from(document.querySelectorAll('.item-tag a')).forEach(item => {
+    item.onclick = function (e) {
+      e.preventDefault();
+      document.querySelector('.item-tag a[class="active"]').classList.remove('active');
+      this.classList.add('active');
+    }
+  })
+}
 
 // reserve page 跳轉
 let currentStep = 1;
@@ -113,4 +122,51 @@ function goStep3() {
   document.querySelector('#steps-item1').classList.add('completed');
   document.querySelector('#steps-item2').classList.add('completed');
   document.querySelector('#steps-item3').classList.add('active');
+}
+
+if (document.querySelector('#date')) {
+  const date = document.querySelector('#date');
+  const datepicker = new Datepicker(date, {
+    autohide: true,
+    language: 'zh-TW',
+  });
+}
+
+if (document.querySelector('#expire-date')) {
+  const expireDate = document.querySelector('#expire-date');
+  const expireDatepicker = new Datepicker(expireDate, {
+    autohide: true,
+    format: 'mm/yy',
+    startView: 2,
+    pickLevel: 1,
+    language: 'zh-TW',
+  });
+}
+
+// 信用卡格式限制
+if (document.querySelector('#account')) {
+  let lastLength = 0;
+  let lastValue = '';
+  document.querySelector('#account').addEventListener('input', function () {
+    let value = this.value;
+    this.value = this.value.replace(/[^-\d]/g, '');
+    let length = this.value.length;
+    if (length < 19) {
+      if (length > lastLength) {
+        if (length % 4 === Math.floor(length / 4) - 1) {
+          this.value = this.value + '-';
+          lastLength += 2;
+        } else {
+          lastLength++;
+        }
+      } else {
+        lastLength = length;
+      }
+    } else if (length === 19) {
+    } else {
+      this.value = lastValue;
+    }
+
+    lastValue = this.value;
+  });
 }
