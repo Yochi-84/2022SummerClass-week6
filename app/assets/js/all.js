8695234e721974cedb053eff2c68f146abf74ac6
Chi <yochi080684@gmail.com>
2022-08-26 11:21:33 +0800

    Correction based on feedback

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -14,9 +14,14 @@ let programSwiper = new Swiper(".programSwiper", {
   loop: true,
   slidesPerView: "auto",
   loopedSlides: 17,
-  centeredSlides: false,
   grabCursor: true,
   mousewheel: true,
+  freeMode: true,
+  speed: 1500,
+  autoplay: {
+    delay: 0,
+    waitForTransition: false
+  },
   breakpoints: {
     320: {
       spaceBetween: 8
